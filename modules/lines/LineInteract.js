19c072c0f68b86500b9f162ab73bd07e863e3aca
Vide0Master <signatov2134@gmail.com>
2023-03-23 11:19:28 +0200

    почти готовая языковая система

--- modules/lines/LineInteract.js
+++ modules/lines/LineInteract.js
@@ -2,8 +2,10 @@
 //* Line interaction от VideoMaster
 
 class LineInteract {
-    static returnRandomLineFrom(lineList) {
-        return lineList[Math.floor(Math.random() * lineList.length)]
+    static returnRandomLineFrom(lines_path,lang) {
+        const lineList = require(`./language/${lang}.json`)
+        const line_array = eval(`lineList.${lines_path}`)
+        return line_array[Math.floor(Math.random() * line_array.length)]
     }
     static getLine(num) {
         return lineList[num]
